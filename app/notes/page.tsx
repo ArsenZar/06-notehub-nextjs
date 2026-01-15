c5d405be2b909d208a8814a2d8ecfefbbaf0e83a
ArsenZar <arsenzar@gmail.com>
2026-01-15 14:01:02 +0200

    use client

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,16 +1,26 @@
-// notes/page.tsx
+// app/notes/page.tsx
 
-import { getNotes } from "@/lib/api";
+"use client";
+
+import { useState } from "react";
 import NoteList from "@/components/NoteList/NoteList";
+import { getNotes, Note } from "@/lib/api";
 
-const Notes = async () => {
-    const response = await getNotes();
-    console.log(response);
+const Notes = () => {
+    const [notes, setNotes] = useState<Note[]>([]);
+
+    const handleClick = async () => {
+        const response = await getNotes();
+        if (response?.notes) {
+            setNotes(response.notes);
+        }
+    };
 
     return (
         <section>
             <h1>Notes List</h1>
-            {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+            <button onClick={handleClick}>Get my notes</button>
+            {notes.length > 0 && <NoteList notes={notes} />}
         </section>
     );
 }
